e7e6232da449b1819f67383b8959448cd1a7042f
zsrjim <55557022+zsrjim@users.noreply.github.com>
2024-06-13 01:16:36 +0800

    Update diy-part.sh

--- build/Lede_lxc/diy-part.sh
+++ build/Lede_lxc/diy-part.sh
@@ -14,14 +14,14 @@ cat >> ${FILE_DEFAULT_UCI} <<-EOF
 #uci set network.lan.ifname='eth0 eth1'                         # 设置lan口物理接口为eth0、eth1
 #uci set network.lan.ifname='eth0'                              # 设置lan口物理接口为eth0
 uci set network.lan.proto='static'                              # lan口静态IP
-uci set network.lan.ipaddr='192.168.1.2'                        # IPv4 地址(openwrt后台地址)
+uci set network.lan.ipaddr='192.168.50.252'                        # IPv4 地址(openwrt后台地址)
 uci set network.lan.netmask='255.255.255.0'                     # IPv4 子网掩码
-uci set network.lan.gateway='192.168.1.1'                       # IPv4 网关
-uci set network.lan.broadcast='192.168.1.255'                   # IPv4 广播
-uci set network.lan.dns='223.5.5.5 114.114.114.114'             # DNS(多个DNS要用空格分开)
+uci set network.lan.gateway='192.168.50.253'                       # IPv4 网关
+uci set network.lan.broadcast='192.168.50.255'                   # IPv4 广播
+uci set network.lan.dns='223.5.5.5 119.29.29.29'             # DNS(多个DNS要用空格分开)
 #uci set network.lan.mtu='1492'                                 # lan口mtu设置为1492
 #uci set network.lan.delegate='0'                               # 去掉LAN口使用内置的 IPv6 管理
-#uci delete network.lan.ip6assign                               # 接口→LAN→IPv6 分配长度——关闭，恢复uci set network.lan.ip6assign='64'
+uci delete network.lan.ip6assign                               # 接口→LAN→IPv6 分配长度——关闭，恢复uci set network.lan.ip6assign='64'
 uci commit network
 
 #uci delete dhcp.lan.ra                                         # 路由通告服务，设置为“已禁用”
@@ -32,7 +32,7 @@ uci commit network
 #uci set dhcp.@dnsmasq[0].cachesize='0'                         # DHCP/DNS→高级设置→DNS 查询缓存的大小——设置为'0'
 #uci add dhcp domain
 #uci set dhcp.@domain[0].name='openwrt'                         # 网络→主机名→主机目录——“openwrt”
-#uci set dhcp.@domain[0].ip='192.168.1.2'                       # 对应IP解析——192.168.1.2
+#uci set dhcp.@domain[0].ip='192.168.50.2'                       # 对应IP解析——192.168.1.2
 #uci add dhcp domain
 #uci set dhcp.@domain[1].name='cdn.jsdelivr.net'                # 网络→主机名→主机目录——“cdn.jsdelivr.net”
 #uci set dhcp.@domain[1].ip='104.16.86.20'                      # 对应IP解析——'104.16.86.20'
@@ -41,7 +41,7 @@ uci commit network
 #uci set dhcp.@domain[2].ip='185.199.109.133'                   # 对应IP解析——'185.199.109.133'
 uci commit dhcp
 
-#uci delete firewall.@defaults[0].syn_flood                     # 防火墙→SYN-flood 防御——关闭；默认开启
+uci delete firewall.@defaults[0].syn_flood                     # 防火墙→SYN-flood 防御——关闭；默认开启
 #uci set firewall.@defaults[0].fullcone='1'                     # 防火墙→FullCone-NAT——启用；默认关闭
 uci commit firewall
 
@@ -61,7 +61,7 @@ EOF
 if [[ -n "${ZZZ_PATH}" ]]; then  
 	echo '增加个性名字 ${GITHUB_ACTOR} 默认为你的github帐号'
 	# sed -i "s/OpenWrt ${GITHUB_ACTOR} compiled in $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" ${ZZZ_PATH}
-	sed -i "s/OpenWrt /Ss. compiled in $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" ${ZZZ_PATH}
+	sed -i "s/OpenWrt /天天炸 compiled in $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" ${ZZZ_PATH}
 fi
 
 # x86机型,默认内核6.1，修改内核为6.1
